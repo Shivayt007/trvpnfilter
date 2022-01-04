41604fef03a3de069e89338d812cc671dc1252e6
Shivayt007 <88087547+Shivayt007@users.noreply.github.com>
2022-01-04 18:47:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/8af59ac8a12110eaec85d.jpg https://telegra.ph/file/8af59ac8a12110eaec85d.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/c9693d5c27e59e46a0e09.jpg https://telegra.ph/file/b08dfad643a5326732d27.jpg')).split()
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
